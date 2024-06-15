9419980f7583d334d4d21154872ee3fb0da1f92b
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-06-15 00:04:33 -0500

    Add files via upload

---
+++ suma3/src/main/java/com/mycompany/suma3/Suma3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.suma3;
+
+/**
+ *
+ * @author john
+ */
+public class Suma3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
